40d05e8905e4d002f9098c49e87977540f135223
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2017-05-23 15:58:47 -0700

    Frame

--- Moba.java
+++ Moba.java
@@ -1,3 +1,29 @@
-public class Moba {
-	System.out.println("First line of code in the new MOBA");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Moba extends JComponent implements MouseListener{
+
+  static JFrame frame = new JFrame();
+
+  public Moba(){
+    addMouseListener(this);
+  }
+  public void mouseClicked(MouseEvent e){}
+  public void mouseEntered(MouseEvent e){}
+  public void mouseExited(MouseEvent e){}
+  public void mousePressed(MouseEvent e){}
+  public void mouseReleased(MouseEvent e){}
+  public static void main(String[] args){
+
+    Moba game = new Moba();
+
+    frame.add(game);
+    frame.setSize(800, 600);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.setVisible(true);
+    frame.setLocationRelativeTo(null);
+
+  }
 }
